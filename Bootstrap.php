329f36c12e01ebaa40f951d7c678ac041a380fa3
ashraful <ashraful.pust7@gmail.com>
2020-05-07 16:40:15 +0600

    Basic login system added

---
+++ Bootstrap.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html> 
+<html>
+	<head> 
+		<title>Bootstrap 101 Template</title> 
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+		<link href="css/bootstrap.min.css" rel="stylesheet"> 
+		<link rel="stylesheet" href="css/style.css">
+	</head> 
+	<body> 
+		
+
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4 col-md-offset-4 col-xs-4 " style="background-color: red">
+					<p>Hey Dummy 4</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-4 col-md-offset-4 col-xs-4 mt" style="background-color: green">
+					<p>Hey Dummy 4</p>
+				</div>
+			</div>
+			<!-- <div class="col-md-4 col-xs-4" style="background-color: green">
+				<p>Hey Dummy 8</p>
+			</div> </br>
+			<div class="col-md-4 col-xs-4" style="background-color: blue">
+				<p>Hey Dummy 12</p>
+			</div> -->
+		</div>
+
+
+
+		<script src="js/jquery-3.2.1.min.js"></script> 
+	</body> 
+</html>
---
+++ Tricks.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html> 
+<html>
+	<head> 
+		<title>Tricks</title> 
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+		<link href="css/bootstrap.min.css" rel="stylesheet"> 
+		<link rel="stylesheet" href="css/index.css">
+	</head> 
+	<body> 
+		
+
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4 col-md-offset-4 col-xs-4 " style="background-color: red">
+					<ul>
+						<li><a href="Index.php">Home</a></li>
+						<li><a href="Tricks.php">Tricks</a></li>
+						<li><a href="#">Contact</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-4 col-md-offset-4 col-xs-4 mt" style="background-color: green">
+					<p>Hey Dummy 4</p>
+				</div>
+			</div>
+			<!-- <div class="col-md-4 col-xs-4" style="background-color: green">
+				<p>Hey Dummy 8</p>
+			</div> </br>
+			<div class="col-md-4 col-xs-4" style="background-color: blue">
+				<p>Hey Dummy 12</p>
+			</div> -->
+		</div>
+
+
+
+		<script src="js/jquery-3.2.1.min.js"></script> 
+	</body> 
+</html>
